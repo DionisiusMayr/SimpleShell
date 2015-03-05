7b44317cf86c8285497a50e891a4ac9eaf1d9a53
Dionisius Mayr <dmayr@uwo.ca>
2015-03-05 00:45:17 -0500

    README added

--- shell.cpp
+++ shell.cpp
@@ -1,5 +1,5 @@
 /* 	Author: Dionisius Mayr
- * 	Date: 06 Feb, 2015
+ * 	Date: 04 March, 2015
  * 	This program implements a simple UNIX-shell that is able to execute commands given by the user,
  * 	with redirections and up to three pipes. It also has the command exit and history implemented.
  *	It is possible to compile this under a UNIX environment simply using:
